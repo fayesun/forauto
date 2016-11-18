c584dd1cf38fd71d94ab54576ca7c66e4a6f8686
root <root@wdcoud.cc>
2016-11-18 16:42:06 +0800

    add forms.py, form class, newrepl.html -- 2

--- mysqltools/forms.py
+++ mysqltools/forms.py
@@ -1,4 +1,7 @@
 from django import forms
 
 class MysqlReplFrom(forms.Form):
-    pass
+    ip_addr = forms.GenericIPAddressField(label='ip address')
+    tcp_port = forms.CharField(label='port', max_length=5)
+    admin_name = forms.CharField(label='admin', max=10)
+    admin_pass = forms.CharField(label='password', max_length=20)
